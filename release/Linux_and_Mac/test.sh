8c2dc9d3439f9141a37b2ac475be1ae6d2d12364
krarm2209 <krishna.aroomoogon@mail.mcgill.ca>
2023-09-18 19:30:31 -0400

    Updated test.sh

--- release/Linux_and_Mac/test.sh
+++ release/Linux_and_Mac/test.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Get the full directory of the running script
 SCRIPT_DIR="$( cd -- "$(dirname "$0")" > /dev/null 2>&1 ; pwd -P )"
 
+# Get the root directory of the repo
+ROOT_DIR="$(cd -- "$SCRIPT_DIR/../.."; pwd -P )"
+
+# Check if python3.9 is installed
 PYTHON_EXE="python"
-
 PYTHON_VERSION=$("$PYTHON_EXE" --version 2>&1)
-
 $(echo "$PYTHON_VERSION" | grep -q "3.9")
 if [ "$?" -ne 0 ]; then
     PYTHON_EXE="python3"
@@ -17,14 +20,16 @@ if [ "$?" -ne 0 ]; then
     fi
 fi
 
-cd "$SCRIPT_DIR/.."
+# Change directory to root dir
+cd "$ROOT_DIR"
 
+# Create and activate virtual env
 $("$PYTHON_EXE" -m venv ./venv)
-
 . "./venv/bin/activate"
 
-cd "../dist"
-
-WHEEL=$(ls | grep *.whl)
-
+# Get .whl file and install project
+WHEEL=$(find . -name "*.whl")
 pip install "$WHEEL"
+
+# Open the GUI
+automated_cilia_measurements_gui
